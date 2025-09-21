eca3c3163776486cd16a9af87f6485e24c0fed44
sakkoumhamza <hamzasakkoum1@gmail.com>
2025-09-21 10:54:40 +0100

    Feat: Added order tables migration file

---
+++ src/main/resources/db/migration/V4__add_order_tables.sql
@@ -0,0 +1,18 @@
+create table orders(
+    id bigint primary key AUTO_INCREMENT,
+    customer_id bigint not null,
+    status varchar(20) not null,
+    created_at datetime default current_timestamp not null,
+    total_price decimal(10,2) not null,
+    foreign key (customer_id) references users(id)
+);
+create table order_items(
+        id bigint primary key AUTO_INCREMENT,
+       order_id bigint not null ,
+       product_id bigint not null ,
+       unit_price decimal(10,2) not null,
+       quantity int not null,
+       total_price decimal(10,2) not null,
+       foreign key (order_id) references orders(id),
+        foreign key (product_id) references products(id)
+);
